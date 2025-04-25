197ce62b4c0075f606bc73eab7424f9e97082b2c
namdev26 <vuhoangnam310104@gmail.com>
2025-04-26 02:52:05 +0700

    test commit

---
+++ _Scrips/UI/UIInventoryPage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryPage : MonoBehaviour
+{
+    [SerializeField] private UIInventoryItem itemPrefab;
+    [SerializeField] private RectTransform contentPanel;
+    List<UIInventoryItem> listOfUIItem = new List<UIInventoryItem>();
+
+    public void InitInventoryUI(int inventorySize)
+    {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel, false);
+
+            uiItem.transform.SetParent(contentPanel);
+            listOfUIItem.Add(uiItem);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
+
+
+//test haha.
